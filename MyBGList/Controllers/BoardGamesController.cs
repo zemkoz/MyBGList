38aed60b2a6ed4fa8827635d8a781d2a1567ae5d
Zoltan Zemko <zoltan.zemko@hartmann.info>
2024-10-04 09:40:43 +0200

    Add API for returning board games.

---
+++ MyBGList/Controllers/BoardGamesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBGList.Model;
+
+namespace MyBGList.Controllers;
+
+[ApiController]
+[Route("api/board-games")]
+public class BoardGamesController(ILogger<BoardGamesController> logger) : ControllerBase
+{
+    [HttpGet(Name = "GetBoardGames")]
+    public IEnumerable<BoardGame> GetBoardGames()
+    {
+        return
+        [
+            new BoardGame() {
+                Id = 1,
+                Name = "Axis & Allies",
+                Year = 1981
+            },
+            new BoardGame() {
+                Id = 2,
+                Name = "Citadels",
+                Year = 2000
+            },
+            new BoardGame() {
+                Id = 3,
+ 
+                Name = "Terraforming Mars",
+                Year = 2016
+            }
+        ];
+    }
+}
