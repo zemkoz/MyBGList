17b92292022327994dbe11337bf495602771bd87
Zoltan Zemko <zoltan.zemko@hartmann.info>
2024-10-17 14:59:55 +0200

    Create entities and configure their relations.

---
+++ MyBGList/Models/ApplicationDbContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyBGList.Models;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<BoardGame> BoardGames => Set<BoardGame>();
+    public DbSet<Domain> Domains => Set<Domain>();
+    public DbSet<Mechanic> Mechanics => Set<Mechanic>();
+    public DbSet<BoardGame2Domain> BoardGames2Domains => Set<BoardGame2Domain>();
+    public DbSet<BoardGame2Mechanic> BoardGames2Mechanics => Set<BoardGame2Mechanic>();
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<BoardGame2Domain>()
+            .HasKey(bg => new { bg.BoardGameId, bg.DomainId });
+
+        modelBuilder.Entity<BoardGame2Domain>()
+            .HasOne(e => e.BoardGame)
+            .WithMany(e => e.BoardGames_Domains)
+            .HasForeignKey(e => e.BoardGameId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        modelBuilder.Entity<BoardGame2Domain>()
+            .HasOne(e => e.Domain)
+            .WithMany(e => e.BoardGame2Domain)
+            .HasForeignKey(e => e.DomainId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        
+        modelBuilder.Entity<BoardGame2Mechanic>()
+            .HasKey(bg => new { bg.BoardGameId, bg.MechanicId });
+
+        modelBuilder.Entity<BoardGame2Mechanic>()
+            .HasOne(e => e.BoardGame)
+            .WithMany(e => e.BoardGames_Mechanics)
+            .HasForeignKey(e => e.BoardGameId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<BoardGame2Mechanic>()
+            .HasOne(e => e.Mechanic)
+            .WithMany(e => e.BoardGame2Mechanic)
+            .HasForeignKey(e => e.MechanicId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ MyBGList/Models/BoardGame2Mechanic.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyBGList.Models;
+
+[Table("BoardGames_Mechanics")]
+public class BoardGame2Mechanic
+{
+    [Key]
+    [Required]
+    public int BoardGameId { get; set; }
+
+    [Key]
+    [Required]
+    public int MechanicId { get; set; }
+
+    [Required]
+    public DateTime CreatedDate { get; set; }
+    
+    public BoardGame? BoardGame { get; set; }
+    public Mechanic? Mechanic { get; set; }
+}
